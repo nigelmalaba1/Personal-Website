0289ac0b56e25fb73c0f1a1e3064add37b7c613f
EC2 Default User <ec2-user@ip-172-31-60-25.ec2.internal>
2023-03-26 16:40:54 +0000

    message

---
+++ my-website-backend/src/main.rs
@@ -0,0 +1,41 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Project {
+    name: String,
+    description: String,
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Welcome to my GitHub portfolio!")
+}
+
+#[get("/projects")]
+async fn projects() -> impl Responder {
+    let project_list = vec![
+        Project {
+            name: "Project 1".to_string(),
+            description: "Description for Project 1".to_string(),
+        },
+        Project {
+            name: "Project 2".to_string(),
+            description: "Description for Project 2".to_string(),
+        },
+    ];
+
+    HttpResponse::Ok().json(project_list)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(projects)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
